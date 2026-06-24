8c5ad6dba016d268365d5922855b661e4c1f0771
konstantin.geidarov <konstantin.geidarov@indusoft.ru>
2026-06-24 17:24:03 +0300

    feat: BaseRepo

---
+++ core/db/engine.py
@@ -0,0 +1,7 @@
+﻿from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from config import DB_URI
+
+engine = create_async_engine(DB_URI, echo=False)
+
+# async_sessionmaker — более современный вариант вместо sessionmaker
+AsyncSessionFactory = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
---
+++ core/db/repositories/base.py
@@ -0,0 +1,33 @@
+﻿from typing import TypeVar, Generic, Type
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.db import Base
+
+Model = TypeVar("Model", bound=Base)
+
+class BaseRepository(Generic[Model]):
+    model: Type[Model]
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_by_id(self, id: int) -> Model | None:
+        return await self.session.get(self.model, id)
+
+    async def get_all(self) -> list[Model]:
+        result = await self.session.execute(select(self.model))
+        return list(result.scalars().all())
+
+    async def create(self, **kwargs) -> Model:
+        obj = self.model(**kwargs)
+        self.session.add(obj)
+        await self.session.commit()
+        await self.session.refresh(obj)
+        return obj
+
+    async def delete(self, obj: Model) -> None:
+        await self.session.delete(obj)
+        await self.session.commit()
+
