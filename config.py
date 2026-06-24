d91172daaa85eb1407cc435481f5ab784766a578
konstantin.geidarov <konstantin.geidarov@indusoft.ru>
2026-06-24 17:24:37 +0300

    hot: config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+﻿import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_env(name: str):
+    try:
+        return os.environ[name]
+    except KeyError:
+        raise KeyError(f"В .env нет значения {name}")
+
+DB_URI = get_env("DB_URI")
+
+LOG_LEVEL = get_env("LOG_LEVEL")
