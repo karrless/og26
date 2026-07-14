1fae16bf335b3c1ac6649364ca6a0647d6d46bab
konstantin.geidarov <konstantin.geidarov@indusoft.ru>
2026-07-14 13:58:04 +0300

    feat: socks для коннекта

--- tg/bot.py
+++ tg/bot.py
@@ -1,14 +1,20 @@
 ﻿from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import TG_TOKEN
 
+session = AiohttpSession(
+    proxy="socks5://127.0.0.1:51066"
+)
+
 bot = Bot(
     token=TG_TOKEN,
     default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN),
     storage=MemoryStorage(),
+    session=session
 )
 
 storage = MemoryStorage()
