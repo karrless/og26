b94fd8da3659d0960076d90ae29df27cebd08a14
konstantin.geidarov <konstantin.geidarov@indusoft.ru>
2026-08-19 15:46:25 +0300

    feat: add cors

--- api/main.py
+++ api/main.py
@@ -1,8 +1,20 @@
 ﻿from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.auth import router as auth_router
 from api.faq import router as faq_router
 
 app = FastAPI(title="og26 admin API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://og.dev.karrless.ru"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 app.include_router(faq_router)
