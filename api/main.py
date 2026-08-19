55a29c68a8ff5b2acf20ef68b13840a02a179bb5
konstantin.geidarov <konstantin.geidarov@indusoft.ru>
2026-08-19 16:05:08 +0300

    feat: add cors

--- api/main.py
+++ api/main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://og.dev.karrless.ru"
+        FRONT_HOST
     ],
     allow_credentials=True,
     allow_methods=["*"],
